8fd1ea2f2d14d903419a712f2e52526ebc287f36
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-06-07 22:06:13 -0600

    ssss

--- proyecto2Api/business/empBC.php
+++ proyecto2Api/business/empBC.php
@@ -1,9 +1,9 @@
 <?php
-require_once '../data/EmpresaDA.php';
+require_once '../data/empDA.php';
 require_once '../model/Empresa.php';
 include '../business/validation/validationEmpresa.php';
 
-class EmpresaBC {
+class empBC {
 
 
     public function updateEmpresa($empresa, $id) {
@@ -16,16 +16,16 @@ class EmpresaBC {
     }
 
     // Si no hay errores, proceder con la actualización
-    $empresaDA = new EmpresaDA();
+    $empresaDA = new empDA();
     return $empresaDA->updateEmpresa($empresa, $id);
 
 
     }
 
 
-    public function Empresa($empresa, $id) {
+    public function insertEmpresa($empresa) {
         // Validar los campos
-     $mensajeError = validarCampos($empresa);
+     $mensajeError = validarCamposInsert($empresa);
  
      // Si hay un mensaje de error, devolverlo
      if ($mensajeError !== "") {
@@ -33,12 +33,15 @@ class EmpresaBC {
      }
  
      // Si no hay errores, proceder con la actualización
-     $empresaDA = new EmpresaDA();
-     return $empresaDA->updateEmpresa($empresa, $id);
+     $empresaDA = new empDA();
+     return $empresaDA->insertEmpresa($empresa);
  
  
      }
-
+     public function getAllEmpresas() {
+        $empresaDA = new empDA();
+        return $empresaDA->getAllmpresas();
+    }
 
 
 
--- proyecto2Api/controller/empController.php
+++ proyecto2Api/controller/empController.php
@@ -1,7 +1,7 @@
 <?php
 
 include '../bd/BD.php';
-include '../business/EmpresaBC.php';
+include '../business/empBC.php';
 require_once '../model/Empresa.php';
 
 header('Access-Control-Allow-Origin: *');
@@ -41,6 +41,55 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         exit();
         } 
 
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+    if (isset($_POST['insert'])) {
+        unset($_POST['METHOD']);
+        // Actualizar datos de la empresa
+
+        $cedula = $_POST['cedula'];
+        $direccion = $_POST['direccion'];
+        $fecha_creacion = $_POST['fecha_creacion'];
+        $nombre = $_POST['nombre'];
+        $telefono = $_POST['telefono'];
+        $contraseña = $_POST['contraseña'];
+        $correo = $_POST['correo'];
+
+
+        $empresa = new Empresa($nombre,$cedula,$direccion,$fecha_creacion,$telefono,$contraseña,true,$correo);
+
+        $empresaBC = new empBC();
+
+
+
+        $resultado =$empresaBC->insertEmpresa( $empresa);
+
+
+        echo json_encode($resultado);
+
+
+
+        header("HTTP/1.1 200 OK");
+        exit();
+        } 
+
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['allEmpresas'])) {
+        $empresaBC = new empBC();
+        $resultado = $empresaBC->getAllEmpresas();
+        echo json_encode($resultado->fetchAll(PDO::FETCH_ASSOC));
+        header("HTTP/1.1 200 OK");
+    } else {
+        header("HTTP/1.1 400 Bad Request");
+    }
+    exit();
 }
 
 
